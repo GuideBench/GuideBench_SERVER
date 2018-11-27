b52cd4bf58b01a5482d9403ce47d795085b633c6
Hyewon <haha0140@naver.com>
2018-11-28 00:36:33 +0900

    Q&A 완성

--- src/routes/index.js
+++ src/routes/index.js
@@ -23,7 +23,7 @@ const { Router } = require('express')
 const user = require('./user')
 const event = require('./event')
 const qna = require('./qna')
-// jin branch !!!!
+
 
 const router = Router()
 
@@ -32,5 +32,5 @@ const router = Router()
 router.use('/user', user) // localhost:3000/user로 접속하면 이쪽으로 분기처리 됩니다.
 router.use('/event', event)
 router.use('/qna', qna)
-// joo branch
+
 module.exports = router // 현재 js파일을 다른 파일에서도 사용할 수 있도록 export
