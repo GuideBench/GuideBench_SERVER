c39239e4fba5a1300a2941d3194e0580124648eb
jinsol9419 <jinsol9419@naver.com>
2018-12-04 22:17:56 +0900

    app.js modified

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-this.app.get('/*', function(req, res){
+app.get('/*', function(req, res){
   res.sendFile(path.join(__dirname, './..index.html'))
 })
 
