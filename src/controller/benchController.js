65ceb4853f4c824a37766f5c78422590f4f5d17f
jinsol9419 <jinsol9419@naver.com>
2018-12-04 20:59:17 +0900

    cors add

--- src/controller/benchController.js
+++ src/controller/benchController.js
@@ -21,14 +21,14 @@ exports.postBench = async (req, res) => {
   }
 }
 
-exports.benchinfo = async (req, res) => {
+exports.postInfo = async (req, res) => {
   try {
-    const { where, wherename, whereaddress } = req.body
-    if(!where || !wherename || !whereaddress) {
+    const { benchinfo_category, benchinfo_name, benchinfo_address } = req.body
+    if(!benchinfo_category || !benchinfo_name || !benchinfo_address) {
       response.respondOnError('모두 입력해주세요.', res, 400)
     } 
   
-      await qnaService.answer(req) 
+      await benchService.postInfo(req) 
    
       response.respondJson2('벤치의 정보가 성공적으로 등록되었습니다', res, 201) 
     
@@ -37,13 +37,13 @@ exports.benchinfo = async (req, res) => {
   }
 }
 
-exports.getqna = async (req, res) => {
-  try {
-    const result =  await qna.find({})  // 위에 선언한 userService 파일의 signup 함수의 리턴값이 올때까지 기다림
-    response.respondJson2(result, res, 201) // 첫번째 인자는 응답할 message, 두번째 인자는 res (응답 오브젝트에 대한 메소드), 세번째 인자는 status code
-  }  
-   catch (e) {
-    response.respondOnError('Internal Server error', res, 500)
-  }
+// exports.ben = async (req, res) => {
+//   try {
+//     const result =  await qna.find({})  // 위에 선언한 userService 파일의 signup 함수의 리턴값이 올때까지 기다림
+//     response.respondJson2(result, res, 201) // 첫번째 인자는 응답할 message, 두번째 인자는 res (응답 오브젝트에 대한 메소드), 세번째 인자는 status code
+//   }  
+//    catch (e) {
+//     response.respondOnError('Internal Server error', res, 500)
+//   }
   
-}
+// }
