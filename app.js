d104e032f2ec72486a1b1c1b156d7551c37b24b6
jinsol9419 <jinsol9419@naver.com>
2018-12-04 22:46:17 +0900

    app.js modified

--- app.js
+++ app.js
@@ -24,10 +24,11 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter)
 app.get('/*', function(req, res){
   res.sendFile(path.join(__dirname, './public/index.html'))
 })
+app.use('/', indexRouter)
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
