e9dd0363d44b0830d512c0533cd06913d3d0aabf
jinsol9419 <jinsol9419@naver.com>
2018-12-04 23:21:47 +0900

    app.js modified

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/*', function(req, res){
+app.get('/', function(req, res){
   res.sendFile(path.resolve(__dirname, 'public/index.html'))
 })
 app.use('/', indexRouter)
